4ebcb0647104a29841d9cbc845633700167fa77f
squesadav <sebas.quesada790@gmail.com>
2022-08-31 02:44:51 -0600

    Creada view y controller para CursoLibre

---
+++ Atestados.UI/Controllers/Atestados/CursoLibreController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Atestados.UI.Controllers.Atestados
+{
+    public class CursoLibreController : Controller
+    {
+        // GET: CursoLibre
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CursoLibre/Crear
+        public ActionResult Crear()
+        {
+            return View();
+        }
+    }
+}
