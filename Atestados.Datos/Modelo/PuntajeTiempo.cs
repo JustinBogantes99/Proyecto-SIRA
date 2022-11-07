30e08d4ce99646c7040168ab83ba7f31516ded25
Justin Bogantes <justin.bogantes99@gmail.com>
2022-11-06 18:50:12 -0600

    Refactorizacion del backend

--- Atestados.Datos/Modelo/PuntajeTiempo.cs
+++ Atestados.Datos/Modelo/PuntajeTiempo.cs
@@ -12,15 +12,15 @@ namespace Atestados.Datos.Modelo
     using System;
     using System.Collections.Generic;
     
-    public partial class TipoPuntaje
+    public partial class PuntajeTiempo
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public TipoPuntaje()
+        public PuntajeTiempo()
         {
             this.Rubrica = new HashSet<Rubrica>();
         }
     
-        public int TipoPuntajeID { get; set; }
+        public int PuntajeTiempoID { get; set; }
         public string Nombre { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
